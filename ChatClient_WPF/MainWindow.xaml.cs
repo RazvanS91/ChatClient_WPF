8880ebac8153bb39837b488cf6ee5e2de9afe88d
RazvanS91 <38920915+RazvanS91@users.noreply.github.com>
2019-01-19 11:10:21 +0200

    Multiple threads now separate the UI from the incoming/outgoing stream
    Also updated the responsiveness of the messages display page and added a notification sound for when new messages are received. Messages can now be sent using either the "send" button or by pressing enter.

--- ChatClient_WPF/MainWindow.xaml.cs
+++ ChatClient_WPF/MainWindow.xaml.cs
@@ -5,24 +5,53 @@ using System.Text;
 using System.Windows;
 using ChatClient;
 using System.Threading;
+using System.Media;
 
 namespace ChatClient_WPF
 {
     public partial class MainWindow : Window
     {
         private Client client;
+        private bool isConnected;
 
         public MainWindow()
         {
             InitializeComponent();
             SendBtn.IsEnabled = false;
             DisconnectBtn.Visibility = Visibility.Hidden;
-            ChatPage.IsEnabled = false;
+            ChatPage.VerticalScrollBarVisibility = System.Windows.Controls.ScrollBarVisibility.Visible;
+            ChatPage.IsReadOnly = true;
         }
 
-        private void Send(object sender, RoutedEventArgs e)
+
+        private void Send_Click(object sender, RoutedEventArgs e)
         {
-            client.SendMessage(Message.Text);
+            ValidateAndSendMessage();
+        }
+
+        private void ValidateAndSendMessage()
+        {
+            if (!String.IsNullOrWhiteSpace(Message.Text))
+                client.SendMessage(Message.Text);
+            Message.Text = null;
+            Message.Focus();
+        }
+
+        private void PostMessageOnChatPage(string message)
+        {
+            new SoundPlayer(Properties.Resources.Windows_Unlock).Play();
+            ChatPage.Text += $"{message}\n";
+        }
+        
+        private void GetMessageFromServer()
+        {
+            while (isConnected)
+            {
+                var message = client.ReceiveFromServer();
+                if (ChatPage.LineCount > 20)
+                    Dispatcher.Invoke(() => ChatPage.ScrollToLine(ChatPage.LineCount - 1));
+                Dispatcher.Invoke(() => PostMessageOnChatPage(message));
+            }
         }
 
         private void Connect_Click(object sender, RoutedEventArgs e)
@@ -32,9 +61,13 @@ namespace ChatClient_WPF
                 Connect.IsEnabled = false;
                 SendBtn.IsEnabled = true;
                 client = new Client(Username.Text);
-
+                isConnected = true;
+                new Thread(GetMessageFromServer).Start();
+                
                 SuccessMsg.Text = $"Welcome {Username.Text}. You are connected !";
+                SuccessMsg.Visibility = Visibility.Visible;
                 DisconnectBtn.Visibility = Visibility.Visible;
+                DisconnectBtn.IsEnabled = true;
             }
             else
                 SuccessMsg.Text = "Connection failed. Please retry !";
@@ -42,11 +75,20 @@ namespace ChatClient_WPF
 
         private void Disconnect_Click(object sender, RoutedEventArgs e)
         {
-            client.Disconnect();
+            new Thread(() => client.Disconnect()).Start();
+            isConnected = false;
             DisconnectBtn.IsEnabled = false;
             Connect.IsEnabled = true;
             SendBtn.IsEnabled = false;
             SuccessMsg.Visibility = Visibility.Hidden;
+        }
+
+        private void Message_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter && SendBtn.IsEnabled == true)
+            {
+                ValidateAndSendMessage();
+            }
         }
     }
 }
